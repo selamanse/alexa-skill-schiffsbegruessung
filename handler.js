9a3a474b9659f97c3c31967841d7f079602eae01
Selamanse <selamanse@scheinfrei.info>
2018-08-31 12:25:16 +0200

    Initial commit

---
+++ handler.js
@@ -0,0 +1,138 @@
+/**
+ * This is the code part of the Alexa Skill Schiffsbegrüßung
+ * Author: Selamanse <selamanse@scheinfrei.info>
+ *
+ **/
+
+'use strict'
+
+const AWS = require('aws-sdk')
+const Alexa = require('ask-sdk-core')
+const helper = require('./lib/helper.js')
+const moment = require('moment')
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+const APP_ID = 'amzn1.ask.skill.0dd13499-e6e1-452e-95ea-f4e509ad8ff7'
+
+const messages = {
+  'skillName': 'Alexa Schiffsbegrüßungsanlage',
+  welcome: ' <audio src="https://s3-eu-west-1.amazonaws.com/skillstuff/seagull.mp3" /> Moin Moin und Ahoi! Willkommen bei der Alexa Schiffsbegrüßungsanlage des Hamburger Hafens!',
+  help: 'Du kannst dir Namen von Schiffen ausgeben lassen die gerade im Hamburger-Hafen einlaufen, indem Du sagst "Alexa frage Hamburg Hafen welches Schiff läuft ein"',
+  reprompt: 'Kannst Du das bitte wiederholen?',
+  stop: 'Tschüss, bis zum nächsten mal!'
+}
+
+/* INTENT HANDLERS */
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === `LaunchRequest`;
+  },
+  handle(handlerInput) {
+    const response = handlerInput.responseBuilder;
+    
+    return response.speak(`${messages.welcome} ${messages.help}`)
+      .reprompt(messages.help)
+      .getResponse();
+  }
+};
+
+const GetIncomingShipsHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    console.log("Inside GetIncomingShipsHandler");
+    console.log(JSON.stringify(request));
+    return request.type === "IntentRequest" &&
+           request.intent.name === "GetIncomingShips";
+  },
+  async handle(handlerInput) {
+    console.log("Inside GetIncomingShipsHandler - handle");
+    const request = handlerInput.requestEnvelope.request;
+    const response = handlerInput.responseBuilder;
+    console.log('Tryblock')
+    let outputSpeech = '';
+    
+    try {
+      
+      const shipName = await helper.getFirstPassengerShipName()
+      console.dir(shipName)
+      outputSpeech = `Derzeit läuft das Schiff ${shipName} im Hamburger Hafen ein.`
+      return response
+        .speak(outputSpeech)
+        .withSimpleCard(outputSpeech , 'Ahoi!')
+        .getResponse();
+    }catch (error) {
+      outputSpeech = `Es tut mir leid. Es gab einen Fehler bei der Abfrage der Anzahl der Produkte im Shop ${shopEnvironment}`;
+      console.log(`Intent: ${handlerInput.requestEnvelope.request.intent.name}: message: ${error.message}`);
+    }
+    
+    return response
+      .speak(outputSpeech)
+      .withSimpleCard(SKILL_NAME, card)
+      .getResponse();
+  }
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    console.log("Inside HelpHandler");
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' &&
+           request.intent.name === 'AMAZON.HelpHandler';
+  },
+  handle(handlerInput) {
+    console.log("Inside HelpHandler - handle");
+    return handlerInput.responseBuilder
+      .speak(messages.help)
+      .reprompt(messages.reprompt)
+      .getResponse();
+  }
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    console.log("Inside ExitHandler");
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === `IntentRequest` && (
+              request.intent.name === 'AMAZON.StopIntent' ||
+              request.intent.name === 'AMAZON.CancelIntent'
+           );
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(messages.stop)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+
+const ErrorHandler = {
+  canHandle() {
+    console.log("Inside ErrorHandler");
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log("Inside ErrorHandler - handle");
+    console.log(`Error handled: ${JSON.stringify(error)}`);
+    console.log(`Handler Input: ${JSON.stringify(handlerInput)}`);
+
+    return handlerInput.responseBuilder
+      .speak('Tut mir leid. Versuche es nochmal!')
+      .getResponse();
+  },
+};
+
+/* LAMBDA SETUP */
+module.exports.handle = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    GetIncomingShipsHandler,
+    HelpHandler,
+    ExitHandler
+  )
+  .withSkillId(APP_ID)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ takeitup.js
@@ -0,0 +1,9 @@
+'use strict'
+const helper = require('./lib/helper.js')
+const util = require('util')
+
+try {
+  helper.getFirstPassengerShipName().then((res) => { console.log(util.inspect(res, false, null)) })
+} catch (error) {
+  console.log(error)
+}
